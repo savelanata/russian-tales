8672fb3fe23082c2828eb011417f5e3caa761a02
savelanata <44321940+savelanata@users.noreply.github.com>
2020-01-12 18:29:17 +0200

    Add files via upload

---
+++ src/components/ShowPage/ShowPage.jsx
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import Text from '../Text/Text';
+import '../GamePage/GamePage.css';
+
+const ShowPage = (props) => {
+     return (
+       <div>
+             <img src={props.bgSrc} className='smallBg' alt=' ' />
+             <Text className='GameName' value={props.textGame.GameName[props.lang]} />
+             <Text className='GameQuestion' value={props.textGame.GameQuestion[props.lang]} />
+
+             <div className='Card1' > <img src={props.src} className='gameImg' alt=' ' /></div>
+             <div className='Card4'  >   </div>
+
+             <div className='AnswerPlace'>{props.gameWord}  </div>
+
+             <div className='GamePrev' onClick={() => props.onClick('prev')} hidden={props.hiddenPrev} > </div>
+             <div className='GameNext' onClick={() => props.onClick('next')}  > </div>
+        </div>
+    );
+    };
+
+export default ShowPage;
