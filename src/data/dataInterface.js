144490a68423e84b57eea403b8ffa55fe654441c
savelanata <44321940+savelanata@users.noreply.github.com>
2020-01-12 18:33:39 +0200

    Add files via upload

---
+++ src/data/dataInterface.js
@@ -0,0 +1,303 @@
+const textContents = {
+    "startPage": {
+        "Finland": {"fin": "suomeksi", "rus": "Финский" },
+        "Russia": { "fin": "venäjäksi", "rus": "Русский" },
+        "Start": { "fin": "Aloita peli", "rus": "Начать игру" },
+        "Welcome": { "fin": "Valitse pelin kieli", "rus": "Выбери язык игры" }
+    },
+    "topMenu": {
+        "NavbarFi": { "fin": "Su", "rus": "Фи" },
+        "NavbarRu": { "fin": "Ve", "rus": "Ру" },
+        "NavbarClose": { "fin": "X", "rus": "X" },
+        "NavbarInfo": { "fin": "?", "rus": "?" },
+        "GameTitle": { "fin": "Opimme verbejä", "rus": "Учим глаголы" }
+    },
+    "textGame": {
+        "GameName": { "fin": ["Opitaan verbejä venäläisten satujen sankarien kanssa"], "rus": ["Учим глаголы", "с героями русских сказок"] },
+        "GameQuestion": { "fin": "Mitä satusankari tekee? ", "rus": "Что делает герой сказки?" },
+        "Yes": {  "fin": "Kyllä", "rus": "ДА" },
+        "No": { "fin": "Ei", "rus": "НЕТ" },
+        "WellDone": { "fin": "Hyvin tehty!", "rus": "Правильно!" },
+        "BadAnswer": {  "fin": "Väärin!", "rus": "Ошибка!" },
+        "HowToPlay": { "fin": "Kuinka pelata", "rus": "Как играть" },
+        "HowToPlayText": {
+            "fin": ["Ensimmäisessä ruudussa on kaksi korttia: ", "toinen on auki, toinen ei.", "Avoimessa kortissa on ", "temaattinen kuva sadun sankarista.", "Suljetussa kortissa on verbi, ", "joka voi osoittaa sankarin toiminnan ", "tai saattaa sisältää väärän vastauksen.", "Jos vastaus on oikea, ", "paina kursorilla ”Kyllä” -painiketta,", "jos kortin verbi ei ole oikea – ”Ei” -painiketta.", "Nuolilla ”Takaisin” ja ”Eteenpäin” ", "voit palata katsomaan suoritettuja tehtäviä.", "Peli sisältää 45 teemaattista korttia aiheesta ”Verbi”.", "", "Onnea peliin!"],
+            "rus": ["На первом экране расположены две карточки:", "одна открыта, другая – нет.", "На открытой карточке тематическая ", "картинка с героем из сказки.", "На закрытой – глагол, который может правильно ", "обозначать действие героя, а может содержать", "неправильный ответ. Если ответ правильный,", "нажми курсором на кнопку «ДА»,", "если глагол на карточке неправильный –", " на кнопку «НЕТ».", "По стрелочкам «Назад» и «Вперёд»", "можно вернуться и просмотреть", "выполненные задания.", "В игре содержится 45 тематических карточек", "по теме «Глагол». ", "Удачи в игре!"]
+        },
+        "PlayAgain": {"fin": "Aloita alusta", "rus": "Начать с начала" },
+        "Continue": {"fin": "Jatka", "rus": "Продолжить" }
+    },
+    "content": {
+        "Kataetcja": {
+            "fin": "AJELEE",
+            "rus": "КАТАЕТСЯ",
+            "finWrong": "ajelee",
+            "rusWrong": "катается"
+        },
+        "Kachaetcja": {
+            "fin": "KEINUU",
+            "rus": "КАЧАЕТСЯ",
+            "finWrong": "keinu",
+            "rusWrong": "качается"
+        },
+        "Kopaet": {
+            "fin": "KAIVAA",
+            "rus": "КОПАЕТ",
+            "finWrong": "kaiva",
+            "rusWrong": "копает"
+        },
+        "Rybachit": {
+            "fin": "KALASTAA",
+            "rus": "РЫБАЧИТ",
+            "finWrong": "kalasta",
+            "rusWrong": "рыбачит"
+        },
+        "Stoit": {
+            "fin": "NOJAA",
+            "rus": "облокачивается",
+            "finWrong": "nojaa",
+            "rusWrong": "облокачивается"
+        },
+        "Vhodit": {
+            "fin": ["TULEE", " SISÄÄN"],
+            "rus": "ВХОДИТ",
+            "finWrong": "tule sisään",
+            "rusWrong": "входит"
+        },
+        "Vyhodit": {
+            "fin": ["TULEE", " ULOS "],
+            "rus": "ВЫХОДИТ",
+            "finWrong": "tule ulos",
+            "rusWrong": "выходит"
+        },
+        "Govorit": {
+            "fin": "PUHUU",
+            "rus": "ГОВОРИТ",
+            "finWrong": "puhu",
+            "rusWrong": "говорит"
+        },
+        "Igraet": {
+            "fin": "SOITTAA",
+            "rus": "ИГРАЕТ",
+            "finWrong": "soita",
+            "rusWrong": "играет"
+        },
+        "Idet": {
+            "fin": "KÄVELEE",
+            "rus": "ИДЁТ",
+            "finWrong": "kävele",
+            "rusWrong": "идёт"
+        },
+        "Darit": {
+            "fin": "ANTAA",
+            "rus": "ДАРИТ",
+            "finWrong": "anna",
+            "rusWrong": "дарит"
+        },
+        "Laet": {
+            "fin": "HAUKKUU",
+            "rus": "ЛАЕТ",
+            "finWrong": "hauku",
+            "rusWrong": "лает"
+        },
+        "Raspahivaet": {
+            "fin": ["AVAA", " LEVÄLLEEN"],
+            "rus": "РАСПАХИВАЕТ",
+            "finWrong": "avaa levälleen",
+            "rusWrong": "распахивает"
+        },
+        "Sidit": {
+            "fin": "ISTUU",
+            "rus": "СИДИТ",
+            "finWrong": "istu",
+            "rusWrong": "сидит"
+        },
+        "Bezit": {
+            "fin": "JUOKSEE",
+            "rus": "БЕЖИТ",
+            "finWrong": "juokse",
+            "rusWrong": "бежит"
+        },
+        "Poet": {
+            "fin": "LAULAA",
+            "rus": "ПОЁТ",
+            "finWrong": "laula",
+            "rusWrong": "поёт"
+        },
+        "Pomogaet": {
+            "fin": "AUTTAA",
+            "rus": "ПОМОГАЕТ",
+            "finWrong": "auta",
+            "rusWrong": "помогает"
+        },
+        "Pokazivaet": {
+            "fin": "NÄYTTÄÄ",
+            "rus": "ПОКАЗЫВАЕТ",
+            "finWrong": "näytä",
+            "rusWrong": "показывает"
+        },
+        "Strogaet": {
+            "fin": "HÖYLÄÄ",
+            "rus": "СТРОГАЕТ",
+            "finWrong": "höylää",
+            "rusWrong": "строгает"
+        },
+    "Vygljadyvaet": {
+        "fin": ["KURKISTAA", " ULOS"],
+        "rus": "ВЫГЛЯДЫВАЕТ",
+        "finWrong": "kurkista ulos",
+        "rusWrong": "выглядывает"
+        },
+        "Visit": {
+            "fin": "RIIPPUU",
+            "rus": "ВИСИТ",
+            "finWrong": "riipu",
+            "rusWrong": "висит"
+        },
+        "Vytiraet": {
+            "fin": "PYYHKII",
+            "rus": "ВЫТИРАЕТСЯ",
+            "finWrong": "pyyhkii",
+            "rusWrong": "вытирается"
+        },
+"Zagljadyvaet": {
+    "fin": ["KURKISTAA", " SISÄÄN"],
+    "rus": "ЗАГЛЯДЫВАЕТ",
+    "finWrong": "kurkista sisään",
+    "rusWrong": "заглядывает"
+        },
+        "Zvonit": {
+            "fin": "SOITTAA",
+            "rus": "ЗВОНИТ",
+            "finWrong": "soita",
+            "rusWrong": "звонит"
+        },
+        "Kosit": {
+            "fin": "NIITTÄÄ",
+            "rus": "КОСИТ",
+            "finWrong": "niitä",
+            "rusWrong": "косит"
+        },
+        "Slushaet": {
+            "fin": "KUUNTELEE",
+            "rus": "СЛУШАЕТ",
+            "finWrong": "kuuntele",
+            "rusWrong": "слушает"
+        },
+        "Tancuet": {
+            "fin": "TANSSII",
+            "rus": "ТАНЦУЕТ",
+            "finWrong": "tanssi",
+            "rusWrong": "танцует"
+        },
+        "Tashit": {
+            "fin": "VETÄÄ",
+            "rus": "ТАЩИТ",
+            "finWrong": "vedä",
+            "rusWrong": "тащит"
+        },
+        "Chertit": {
+            "fin": "PIIRTÄÄ",
+            "rus": "ЧЕРТИТ",
+            "finWrong": "piirrä",
+            "rusWrong": "чертит"
+        },
+        "Veshaet": {
+            "fin": "RIPUSTAA",
+            "rus": "ВЕШАЕТ",
+            "finWrong": "ripusta",
+            "rusWrong": "вешает"
+        },
+        "Obedaet": {
+    "fin": ["SYÖ", " LOUNASTA"],
+            "rus": "ОБЕДАЕТ",
+            "finWrong": "syö lounasta",
+            "rusWrong": "обедает"
+        },
+        "Plachet": {
+            "fin": "ITKEE",
+            "rus": "ПЛАЧЕТ",
+            "finWrong": "itke",
+            "rusWrong": "плачет"
+        },
+        "Podmetaet": {
+            "fin": "LAKAISEE",
+            "rus": "ПОДМЕТАЕТ",
+            "finWrong": "lakaise",
+            "rusWrong": "подметает"
+        },
+        "Pozdravljaet": {
+            "fin": "ONNITTELEE",
+            "rus": "ПОЗДРАВЛЯЕТ",
+            "finWrong": "onnittele",
+            "rusWrong": "поздравляет"
+        },
+        "Sobiraet": {
+            "fin": "KERÄÄ",
+            "rus": "СОБИРАЕТ",
+            "finWrong": "kerää",
+            "rusWrong": "собирает"
+        },
+        "Lezit": {
+            "fin": "MAKAA",
+            "rus": "ЛЕЖИТ",
+            "finWrong": "makaa",
+            "rusWrong": "лежит"
+        },
+        "Neset": {
+            "fin": "KANTAA",
+            "rus": "НЕСЁТ",
+            "finWrong": "kanna",
+            "rusWrong": "несёт"
+        },
+        "Pishet": {
+            "fin": "KIRJOITTAA",
+            "rus": "ПИШЕТ",
+            "finWrong": "kirjoita",
+            "rusWrong": "пишет"
+        },
+        "Shjet": {
+            "fin": "OMPELEE",
+            "rus": "ШЬЁТ",
+            "finWrong": "ompele",
+            "rusWrong": "шьёт"
+        },
+        "Vedet": {
+            "fin": "TALUTTAA",
+            "rus": "ВЕДЁТ",
+            "finWrong": "taluta",
+            "rusWrong": "ведёт"
+        },
+        "Dostaet": {
+            "fin": "OTTAA",
+            "rus": "ДОСТАЁТ",
+            "finWrong": "ota",
+            "rusWrong": "достаёт"
+        },
+        "Chitaet": {
+            "fin": "LUKEE",
+            "rus": "ЧИТАЕТ",
+            "finWrong": "lue",
+            "rusWrong": "читает"
+        },
+        "Vjazet": {
+            "fin": "NEULOO",
+            "rus": "ВЯЖЕТ",
+            "finWrong": "neulo",
+            "rusWrong": "вяжет"
+        },
+        "Grebet": {
+            "fin": "SOUTAA",
+            "rus": "ГРЕБЁТ",
+            "finWrong": "souda",
+            "rusWrong": "гребёт"
+        },
+        "Est": {
+            "fin": "SYÖ",
+            "rus": "ЕСТ",
+            "finWrong": "syö",
+            "rusWrong": "ест"
+        }
+    }
+}
+export default textContents;
